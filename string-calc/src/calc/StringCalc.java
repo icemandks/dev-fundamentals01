b21d92dc408ce6ca05e9a647d28c287602797a9f
Mauricio Ramirez <mauricio.ramirez.f@gmail.com>
2018-09-04 14:03:48 -0400

    added calc hw

---
+++ string-calc/src/calc/StringCalc.java
@@ -0,0 +1,9 @@
+package calc;
+
+public class StringCalc {
+	
+	public int sum(String text) {
+//		List<String> digits = Arrays.asList(text.split(","));
+		return 0;
+	}
+}
---
+++ string-calc/src/calc/StringCalcTest.java
@@ -0,0 +1,46 @@
+package calc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StringCalcTest {
+
+	@Test
+	public void StringCalc_sum_testEmpty() {
+		String text = "";
+		int expected = 0;
+		StringCalc calc = new StringCalc();
+		
+		Assert.assertEquals(expected, calc.sum(text));
+	}
+
+	@Test
+	public void StringCalc_sum_test1argument() {
+		String text = "1";
+		int expected = 1;
+		StringCalc calc = new StringCalc();
+		
+		Assert.assertEquals(expected, calc.sum(text));
+	}
+
+	@Test
+	public void StringCalc_sum_test2arguments() {
+		String text = "1,2";
+		int expected = 3;
+		StringCalc calc = new StringCalc();
+		
+		Assert.assertEquals(expected, calc.sum(text));
+	}
+
+	@Test(expected = Exception.class)
+	public void StringCalc_sum_test3argument() {
+		String text = "1,3,4";
+		int expected = 0;
+		StringCalc calc = new StringCalc();
+		
+		Assert.assertEquals(expected, calc.sum(text));
+	}
+
+}
