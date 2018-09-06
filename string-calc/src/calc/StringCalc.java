7efb49de0c407aab476331a4136966dc581ae62e
maramirez <bqemauri@us.ibm.com>
2018-09-06 19:19:34 -0400

    added custom delimiter functionallity

--- string-calc/src/calc/StringCalc.java
+++ string-calc/src/calc/StringCalc.java
@@ -6,15 +6,40 @@ import java.util.List;
 public class StringCalc {
 	private List<Integer> inputs = new ArrayList<Integer>();
 	private int result = 0;
+	private String regex = "(\n){1}|(,){1}";
+	private List<String> customDelimiters = new ArrayList<String>();
 	
 	public int Add(String text) {
 //		List<String> digits = Arrays.asList(text.split(","));
 		try {
-			String[] digitStrings = text.split(",");
+			if (text.substring(0, 2).equals("//")){
+				text = text.substring(2);
+
+				while(text.substring(0, 1).equals("[")){
+					int closeDelimiter = text.indexOf("]");
+
+					if (closeDelimiter == -1){
+						throw new Exception("Wrong format for custom delimiter, no closing bracket ] for the delimiter");
+					}
+
+					this.customDelimiters.add(text.substring(1, closeDelimiter));
+					text = text.substring(closeDelimiter + 1);
+				}
+
+				if (text.substring(0,1).equals("\n")){
+					this.updateRegex();
+					text = text.substring(1);
+				}
+				else {
+					throw new Exception("Wrong format for custom delimiter, no new line after the delimiters declaration");
+				}
+			}
+
+			String[] digitStrings = text.split(regex);
 
 			for (String digit : digitStrings) {
-				System.out.print(digit);
 				int number = Integer.parseInt(digit);
+
 				if (number < 0) {
 					throw new Exception("Negative numbers are not allowed");
 				}
@@ -22,8 +47,7 @@ public class StringCalc {
 			}
 
 			this.result = this.sum(this.inputs);
-			System.out.print(this.result);
-			// return this.result;
+			// System.out.print(this.result);
 		}
 		catch (Exception e) {
 			/* This is a generic Exception handler which means it can handle
@@ -42,12 +66,17 @@ public class StringCalc {
 		}
 		return total;
 	}
+
+	private void updateRegex() {
+		if (this.customDelimiters.size() > 0) {
+			for (String delimiter : this.customDelimiters) {
+				this.regex += "|(" + delimiter + "){1}";
+			}
+		}
+	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		StringCalc cal = new StringCalc();
-		System.out.print("hola");
-		cal.Add("1,2,55");
-		
+		System.out.print(cal.Add("//[;][-]\n1,2\n55-4-2;5"));
 	}
 }
--- string-calc/src/calc/StringCalcTest.java
+++ string-calc/src/calc/StringCalcTest.java
@@ -7,40 +7,77 @@ import org.junit.Test;
 
 public class StringCalcTest {
 
+	private StringCalc calc = new StringCalc();
+
 	@Test
 	public void StringCalc_sum_testEmpty() {
 		String text = "";
 		int expected = 0;
-		StringCalc calc = new StringCalc();
 		
-		Assert.assertEquals(expected, calc.Add(text));
+		Assert.assertEquals(expected, this.calc.Add(text));
 	}
 
 	@Test
 	public void StringCalc_sum_test1argument() {
 		String text = "1";
 		int expected = 1;
-		StringCalc calc = new StringCalc();
 		
-		Assert.assertEquals(expected, calc.Add(text));
+		Assert.assertEquals(expected, this.calc.Add(text));
 	}
 
 	@Test
 	public void StringCalc_sum_test2arguments() {
 		String text = "1,2";
 		int expected = 3;
-		StringCalc calc = new StringCalc();
 		
-		Assert.assertEquals(expected, calc.Add(text));
+		Assert.assertEquals(expected, this.calc.Add(text));
+	}
+
+	@Test
+	public void StringCalc_sum_test3argument() {
+		String text = "1,3,4";
+		int expected = 8;
+		
+		Assert.assertEquals(expected, this.calc.Add(text));
+	}
+
+	@Test
+	public void StringCalc_sum_testlinebrake() {
+		String text = "1,3\n4\n3\n4,6,1";
+		int expected = 22;
+		
+		Assert.assertEquals(expected, this.calc.Add(text));
+	}
+
+	@Test
+	public void StringCalc_sum_testnarguments() {
+		int n = 50;
+		int expected = 0;
+		String text = "";
+
+		for (int i = 0; i < n; i++) {
+			if (i > 0) text += ",";
+			text += i;
+			expected += i;
+		}
+		System.out.print(text);
+		Assert.assertEquals(expected, this.calc.Add(text));
 	}
 
 	@Test(expected = Exception.class)
-	public void StringCalc_sum_test3argument() {
-		String text = "1,3,4";
-		int expected = 0;
-		StringCalc calc = new StringCalc();
+	public void StringCalc_sum_testnotnumber() {
+		String text = "1,*,4";
+		int expected = 5;
 		
-		Assert.assertEquals(expected, calc.Add(text));
+		Assert.assertEquals(expected, this.calc.Add(text));
+	}
+
+	@Test(expected = Exception.class)
+	public void StringCalc_sum_testlinebrakeandcomma() {
+		String text = "1,3\n,4";
+		int expected = 8;
+		
+		Assert.assertEquals(expected, this.calc.Add(text));
 	}
 
 }
