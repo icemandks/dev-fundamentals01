428e776d4185c77f4842160463fdea34956dad00
Mauricio Ramirez <mauricio.ramirez.f@gmail.com>
2018-09-11 09:23:00 -0400

    more inheritance

---
+++ unit-tests/src/package1/C.java
@@ -0,0 +1,12 @@
+package package1;
+
+public class C {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		A objA = new A();
+		objA.display();
+
+	}
+
+}
---
+++ unit-tests/src/package2/D.java
@@ -0,0 +1,12 @@
+package package2;
+import package1.A;
+
+public class D extends A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		D objD = new D();
+		objD.display();
+	}
+
+}
